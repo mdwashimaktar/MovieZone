b3d04a198f44f8e103fb2641f4c1bb2ab576469d
mdwashimaktar <101576182+mdwashimaktar@users.noreply.github.com>
2025-08-13 23:14:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Movies from './components/Movies'
+
+function App() {
+  
+  return (
+    <>
+      <Movies/>
+    </>
+  )
+}
+
+export default App
